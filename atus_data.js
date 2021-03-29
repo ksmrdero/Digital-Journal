482d7a465dbba4bb06ef8d089d769c16ad1e444f
nhchen9 <nc7@illinois.edu>
2021-03-28 22:24:54 -0500

    adding atus and generated data

---
+++ atus_data.js
@@ -0,0 +1,43 @@
+var national_data_simple = {'Sleep': 4991336,
+    'Personal Care': 489255,
+    'Household Activities': 1509772,
+    'Work': 1517217,
+    'Education': 111260,
+    'Shopping': 278519,
+    'Eating/Drinking': 610921,
+    'Leisure': 3096073,
+    'Athletics': 182640,
+    'Travel': 663316,
+    'Other': 136091
+};
+
+var national_data_simple_ratio = {'Sleep': 0.36737737737737736,
+'Personal Care': 0.03601064299593712,
+'Household Activities': 0.11112377082965325,
+'Work': 0.11167174527468644,
+'Education': 0.008189071424365544,
+'Shopping': 0.02049983807336749,
+'Eating/Drinking': 0.04496562739209798,
+'Leisure': 0.2278803067773656,
+'Athletics': 0.013442854619325201,
+'Travel': 0.04882205735146912,
+'Other': 0.010016707884354943
+};
+
+var national_data_full_ratio = {'Sleep': 0.36737737737737736,
+'Personal Care': 0.03601064299593712,
+'Household Activities': 0.08482614967909087,
+'Caring for others': 0.02629762115056233,
+'Work': 0.10533975151622209,
+'Education': 0.008189071424365544,
+'Shopping': 0.02049983807336749,
+'Government Service': 0.0001635458988400165,
+'Eating/Drinking': 0.04496562739209798,
+'Social': 0.21442177471589238,
+'Athletics': 0.013442854619325201,
+'Religion': 0.00849717364423247,
+'Volunteer': 0.006168447859624332,
+'Phone': 0.00496135841724077,
+'Travel': 0.04882205735146912,
+'Other': 0.010016707884354943
+};
